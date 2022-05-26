901e96caeaeb279ba4f002bac6b52d1c1934179f
Andrew Petersen <fespetersen1990@gmail.com>
2022-05-26 16:53:54 -0600

    working through accessing database and posting the results on the web app

--- Server/index.js
+++ Server/index.js
@@ -4,18 +4,33 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const path = require('path');
 const {SERVER_PORT} = process.env
 const {seed} = require('./seed.js')
-const {} = require('./controller.js')
+const {getPortfolioMain, getPortfolioSpring, getPortfolioSummer, getPortfolioWinter, getPortfolioFall} = require('./controller.js')
+
+
+app.use(express.json())
+app.use(cors())
+
+//portfolio requests//
+app.get('/portfolio_main', getPortfolioMain);
+app.get('/portfolio_winter', getPortfolioWinter);
+app.get('/portfolio_spring', getPortfolioSpring);
+app.get('/portfolio_summer', getPortfolioSummer);
+app.get('/portfolio_fall', getPortfolioFall);
+
+
+
+
+//contact requests//
 
 
 
 
 
-
-
-
+//server//
 app.post('/seed', seed)
 
 app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
