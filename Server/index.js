bb994c57306b9631a36acd89ea27940302f5859e
Andrew Petersen <fespetersen1990@gmail.com>
2022-06-01 11:48:42 -0600

    index.js fixed Publi file

--- Server/index.js
+++ Server/index.js
@@ -3,35 +3,35 @@
 
 require('dotenv').config()
 const express = require('express')
-const app = express()
 const cors = require('cors')
 const path = require('path');
+const app = express()
 const {SERVER_PORT} = process.env
 const {seed} = require('./seed.js')
 const {getPosts, createPost, deletePost} = require('./controller.js')
 
-app.use("/", express.static(path.join(__dirname, "../public")));
+app.use("/", express.static(path.join(__dirname, "../Public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
+    res.sendFile(path.join(__dirname, "../Public/index.html"))
 })
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/portfolio.html"))
+    res.sendFile(path.join(__dirname, "../Public/portfolio.html"))
 })
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.css"))
+    res.sendFile(path.join(__dirname, "../Public/index.css"))
 })
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/services.html"))
+    res.sendFile(path.join(__dirname, "../Public/services.html"))
 })
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/share.html"))
+    res.sendFile(path.join(__dirname, "../Public/share.html"))
 })
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/portfolio.js"))
+    res.sendFile(path.join(__dirname, "../Public/portfolio.js"))
 })
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/share.js"))
+    res.sendFile(path.join(__dirname, "../Public/share.js"))
 })
 
 
